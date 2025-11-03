dc51346b1fd38bd7ad2a7810fa54f2b5f8fb7799
acorbel <antoncor@orange.fr>
2025-11-03 18:15:10 +0100

    echo command

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <ranges>
+#include <set>
 
 std::vector<std::string> split(const std::string& str, char delimiter) {
   std::vector<std::string> tokens;
@@ -15,8 +17,8 @@ std::vector<std::string> split(const std::string& str, char delimiter) {
   return tokens;
 }
 
-void exit_command(const std::string& command) {
-  auto tokens = split(command, ' ');
+void exit_command(std::vector<std::string>& tokens) {
+  // default to successful exit
   if (tokens.size() < 2) {
     exit(0);
   } else if (tokens.size() == 2) {
@@ -27,9 +29,21 @@ void exit_command(const std::string& command) {
       std::exit(0);
     }
   } else {
-    throw std::runtime_error("Error code not handled yet.");
+    std::cerr << "Error code not handled yet\n";
   }
+}
 
+void echo_command(std::vector<std::string>& tokens) {
+  if (tokens.size() < 2) {
+    std::cout << "";
+  } else {
+    // remove "echo" from tokens
+    tokens.erase(tokens.begin());
+    for (const auto& token : tokens) {
+      std::cout << token << " ";
+    }
+    std::cout << "\n";
+  }
 }
 
 int main() {
@@ -37,16 +51,26 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::vector<std::string_view> supported_commands{"exit"};
+  std::set<std::string_view> supported_commands{"exit","echo"};
 
   // Read-Eval-Print-Loop (REPL)
   while (true) {
     std::cout << "$ ";
     std::string command;
     std::getline(std::cin, command);
-    if (command.contains("exit")) {
-      exit_command(command);
+    auto tokens = split(command, ' ');
+    auto fn_name = tokens.front();
+
+    if (supported_commands.find(fn_name) == supported_commands.end()) {
+      std::cerr << command << ": not found\n";
     }
-    std::cerr << command << ": command not found\n"; 
+
+    if (fn_name.contains("exit")) {
+      exit_command(tokens);
+    }
+
+    if (fn_name.contains("echo")) {
+      echo_command(tokens);
+    }
   }
 }
